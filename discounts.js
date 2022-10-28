52187a44b2878bc5b97ce761473cf36e93788300
Nicolás Valiconto <nvaliconto@outlook.com>
2022-10-28 19:10:33 -0300

    minor fix

--- discounts.js
+++ discounts.js
@@ -1,22 +1,20 @@
-const btn = document.querySelector('#calculate')
-const inputPrice = document.querySelector('#price')
-const inputDiscount = document.querySelector('#discount')
-const pResult = document.querySelector('#result')
+const btn = document.querySelector("#calculate")
+const inputPrice = document.querySelector("#price")
+const inputDiscount = document.querySelector("#discount")
+const pResult = document.querySelector("#result")
 
 const calculatePriceDiscount = () => {
-    
-    const price = Number(inputPrice.value)
-    const discount = Number(inputDiscount.value)
-    
-    if(!price || !discount) return pResult.innerText = 'We need a price and a discount'
+  const price = Number(inputPrice.value)
+  const discount = Number(inputDiscount.value)
 
-    if(discount >= 100) return pResult.innerText = 'Nothing for free'
-    
-    const newPrice = (price * (100 - discount)) / 100
-    
-    pResult.innerText = `Price with discount: ${newPrice}`
+  if (!price || !discount)
+    return (pResult.innerText = "We need a price and a discount")
+
+  if (discount >= 100) return (pResult.innerText = "Nothing for free")
+
+  const newPrice = (price * (100 - discount)) / 100
+
+  pResult.innerText = `Price with discount: ${newPrice}`
 }
 
-
-
-btn.addEventListener('click', calculatePriceDiscount)
+btn.addEventListener("click", calculatePriceDiscount)
