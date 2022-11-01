5d7923f32d3e331f55bd5ab67c52e538fa3d95fa
Nicolás Valiconto <nvaliconto@outlook.com>
2022-11-01 15:37:25 -0300

    switch

--- discounts.js
+++ discounts.js
@@ -1,16 +1,27 @@
 const btn = document.querySelector("#calculate")
 const inputPrice = document.querySelector("#price")
-const inputDiscount = document.querySelector("#discount")
+const inputCoupon = document.querySelector("#coupon")
 const pResult = document.querySelector("#result")
 
 const calculatePriceDiscount = () => {
   const price = Number(inputPrice.value)
-  const discount = Number(inputDiscount.value)
+  const coupon = inputCoupon.value
+  let discount
 
-  if (!price || !discount)
+  if (!price || !coupon)
     return (pResult.innerText = "We need a price and a discount")
 
-  if (discount >= 100) return (pResult.innerText = "Nothing for free")
+  switch (coupon) {
+    case "Discount_lvl_1":
+      discount = 15
+      break
+    case "Discount_lvl_2":
+      discount = 20
+      break
+    default:
+      pResult.innerText = "Coupon is not valid"
+      return
+  }
 
   const newPrice = (price * (100 - discount)) / 100
 
@@ -18,3 +29,10 @@ const calculatePriceDiscount = () => {
 }
 
 btn.addEventListener("click", calculatePriceDiscount)
+
+// const couponsObj = {
+//   'Discount_lvl_1': 15,
+//   'Discount_lvl_2': 20,
+//   'Discount_lvl_3': 25,
+//   'Discount_lvl_4': 30,
+// }
